6c5bd7117c4e8e91c2bce0c6556a11b989b6df1e
chinekwe01 <chinekwe_clever@outlook.com>
2023-06-11 11:40:45 +0100

    Submit the message form using jquery

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
 </head>
 <body>
     <div id="wrapper">
@@ -17,5 +18,17 @@
             </form>
         </div>
     </div>
+
+    <script>
+        $('.textarea').keyup(function(e){
+            if(e.which == 13){
+                $('form').submit();
+            }
+        });
+        $('form').submit(function(){
+            alert('form is submitted using jquery');
+            return false;
+        });
+    </script>
 </body>
 </html>

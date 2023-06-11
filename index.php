b59078af54a6633725aae5ab8925d58cd42d2716
chinekwe01 <chinekwe_clever@outlook.com>
2023-06-11 11:29:40 +0100

    Designing the homepage for displaying all previous chat and send new messages

--- index.php
+++ index.php
@@ -4,8 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Welcome to my Website</h1>
+    <div id="wrapper">
+        <h1>Welcome to my Website</h1>
+        
+        <div class="chat_wrapper">
+            <div id="chat"></div>
+            <form action="POST">
+                <textarea name="message" cols="30" rows="10" class="textarea"></textarea>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
